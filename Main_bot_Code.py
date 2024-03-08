37c0bcf9bab744ea04c74de47b3ad590164d8f92
Sanskritijaiswal0706 <98076975+Sanskritijaiswal0706@users.noreply.github.com>
2024-03-08 23:10:30 +0530

    Add files via upload

---
+++ Main_bot_Code.py
@@ -0,0 +1,89 @@
+import discord
+from discord.ext import commands, tasks
+import random
+import os 
+from itertools import cycle
+
+
+intents = discord.Intents.default()
+intents.message_content = True
+bot = commands.Bot(command_prefix=">", intents=intents)
+Status = cycle(['Welcome to the The Blue Bird','Share our server link with your friends','HAve a great time here'])
+
+token = "MTAxMzcxNTAyOTk4OTc5Mzg2Mg.G-0pF5.amtTtJdT3sfTU7_sr5mC3TbCx46v2kTiqOXTjY"
+
+@bot.event
+async def on_ready():
+    change_status.start()
+    print("BOT IS ONLINE!!")
+
+@tasks.loop(seconds = 5)
+async def change_status():
+    await bot.change_presence(activity=discord.Game(next(Status)))
+
+@bot.event 
+async def on_command_error(ctx,error):
+    if isinstance(error,commands.CommandNotFound):
+        await ctx.send("Invalid command used")
+
+@bot.event
+async def on_member_join(member):
+    print(f'{member} has joined a server')
+
+@bot.event
+async def  on_member_remove(member):
+    print(f'{member} has been removed form server')
+
+
+#Greeting to bot
+@bot.command(aliases=['hi','hello','Yo','Aye'])
+async def greet(ctx):
+    responses = ["Long time no see",
+                "hello mate!!!",
+                "welcome",
+                "Its good to see you again!!"]
+    await ctx.send(random.choice(responses))
+
+#clear messages
+@bot.command()
+async def clr(ctx,amount : int):
+    await ctx.channel.purge(limit=amount)
+
+@clr.error
+async def clr_error(ctx,error):
+    if isinstance(error,commands.MissingRequiredArgument):
+       await ctx.send("Please specify an amount of messages to delete")
+
+
+
+
+#kicking/banning members
+@bot.command()
+async def kick(ctx, member : discord.Member, *, reason=None):
+    await member.kick(reason=reason)
+
+@bot.command()
+async def ban(ctx, member : discord.Member, *, reason=None):
+    await member.ban(reason=reason)    
+    await ctx.send(f'Banned {member.mention}')
+
+#unbanning member
+@bot.command()
+async def unban(ctx,*,member):
+    banned_users = await ctx.guild.bans()
+    member_name, member_tag = member.split('#')
+
+    for ban_entry in banned_users:
+        user = ban_entry.user
+
+        if (user.name, user.tag) == (member_name,member_tag):
+            await ctx.guild.unban(user)
+            await ctx.send(f'Unbanned {user.mention}')
+            return
+
+
+
+
+      
+
+bot.run(token)
